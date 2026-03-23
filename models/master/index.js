6031d281a0ed4e3a579da8e73abe268420121217
Kavithakanniyappan <kavithait5017@gmail.com>
2026-03-23 12:14:33 +0530

    create racks

--- models/master/index.js
+++ models/master/index.js
@@ -1,52 +1,58 @@
 import mongoose from "mongoose";
 
-//   CUSTOMER SCHEMA
+//CUSTOMER SCHEMA
 const customerSchema = new mongoose.Schema({
-  customer_id: { type: String },
-  customer_name: { type: String },
-  customer_code: { type: String },
-  contact_person: { type: String },
-  contact_number: { type: String },
-  email: { type: String },
-  address: { type: String },
+  customer_id: String,
+  customer_name: String,
+  customer_code: String,
+  contact_person: String,
+  contact_number: String,
+  email: String,
+  address: String,
   status: { type: String, default: "Active" }
 });
 
-//PACK SCHEMA
+// PACK SCHEMA
+
 const packSchema = new mongoose.Schema({
-  type:"PACK",
-  pack: {
-  pack_id: { type: String },
-  part_number:{type:String},
-  part_name: { type: String },
-  customer_id: { type: String },
-  uom: { type: String },
-  description: { type: String },
-  barcode:{type: String},
+  pack_id: String,
+  part_number: String,
+  part_name: String,
+  customer_id: String,
+  uom: String,
+  description: String,
+  barcode: String,
   status: { type: String, default: "Active" }
-},
- racks: [] 
 });
-//Rack Shema
+
+
+  // RACK SCHEMA
+
 const rackSchema = new mongoose.Schema({
-  rack_id: { type: String },
-  pack_id:{type:String},
-  quantity: { type: Number },
-  rack_status:{type:String},
-   is_deleted: { type: Boolean, default: false } 
+  rack_id: String,
+  pack_id: String,
+  quantity: Number,
+  rack_status: String,
+  is_deleted: { type: Boolean, default: false }
 });
+
 
   // MASTER SCHEMA
-
 const masterSchema = new mongoose.Schema({
-  type: { type: String }, // CUSTOMER / PACK
+  type: { 
+    type: String, 
+    enum: ["CUSTOMER", "PACK"], 
+    required: true 
+  },
 
   customer: customerSchema,
   pack: packSchema,
+
+  // ✅ IMPORTANT
   racks: {
-  type: [rackSchema],
-  default: []
-} ,
+    type: [rackSchema],
+    default: []
+  },
 
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now }
